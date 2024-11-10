2b3a00a9aa8513878d53a5fe54650a713e1783e4
Priyanka <ppatil31192@gmail.com>
2024-11-10 23:14:01 +0530

    added submenu

--- routes/v1/menu/menuSchema.js
+++ routes/v1/menu/menuSchema.js
@@ -1,26 +1,35 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var schema = new Schema({
-	Id: {
-		type: mongoose.Types.ObjectId
-	},
-	name: {
-		type: String
-	},
-	file: {
-		type: String
-	},
-	createdBy: {
+const submenuSchema = new Schema({
+    name: { type: String, required: true },
+    url: { type: String, required: true }
+});
+
+var menuSchema = new Schema({
+    Id: {
+        type: mongoose.Types.ObjectId,
+        auto: true  
+    },
+    name: {
+        type: String,
+        required: true 
+    },
+    file: {
+        type: String
+    },
+    submenu: [submenuSchema], 
+    createdBy: {
         type: Schema.Types.ObjectId,
-        ref: 'users'
+        ref: 'users' 
     },
     updatedBy: {
         type: Schema.Types.ObjectId,
         ref: 'users'
     }
-},{
-    timestamps: true
-},
-{strict: false})
-module.exports = mongoose.model('menu', schema);
+}, {
+    timestamps: true,  
+    strict: false      
+});
+
+module.exports = mongoose.model('menu', menuSchema);
